f886dfcb9167a134eeb7fbf2893daeea2d9c5de7
cesaroer <cesarvarcard@gmail.com>
2023-05-26 00:22:39 -0600

    Understanding the react app file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Li = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editar <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li className='clase-li'>valor de Li</li>
+  )
+}
+
+const App = () => {
+  const valor = 'medio medio'
+  return (
+    <ul className="clase-css">
+      <Li></Li>
+    </ul>
   );
 }
 
