069e5e50a002588a3060a288952798e91985fc29
cesaroer <cesarvarcard@gmail.com>
2023-05-26 00:11:18 -0600

    Added some custom elements

--- src/index.js
+++ src/index.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+//const x = React.createElement('h1', null, 'hola')
+
+const Li = ({ children, estado, casa, edad }) => {
+
+  console.log(casa, edad)
+  return (
+    <li>{children} estoy {estado}</li>
+  )
+}
+const X = () =>
+  <ul>
+    <Li
+      estado={'Muy'}
+      casa={false}
+      edad={24}
+    >
+      Feliz
+    </Li>
+    <Li estado={'emocionado'}>Feliz medio</Li>
+    <Li estado={'algo'}>Triste</Li>
+  </ul>
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <X />, document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
