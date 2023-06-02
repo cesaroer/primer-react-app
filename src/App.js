aa4b3e739dc2e245503e0d07f4c2c1852d5ade19
cesaroer <cesarvarcard@gmail.com>
2023-06-02 14:07:19 -0600

    Added styles by js object

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const estilo2 = {
+  boxShadow: '0 5px 3px rgba(0,0,0,0.5',
+};
+
+const estilo = ({ bg = '#733' }) => ({
+  backgroundColor: bg,
+  color: '#fff',
+  padding: '10px 15px',
+  margin: '10px 15px'
+});
+
 const Li = () => {
   return (
-    <li className='clase-li'>valor de Li</li>
+    <li style={{ ...estilo2, ...estilo({ bg: "#533" }) }} className='clase-li'>valor de Li</li>
   )
 }
 
 const App = () => {
   const valor = 'medio medio'
   return (
-    <ul className="clase-css">
+    <ul style={estilo({ bg: '#050' })} className="clase-css">
       <Li></Li>
     </ul>
   );
