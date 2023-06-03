d17adb9025cb678b097450c2563f5999acd9c576
cesaroer <cesarvarcard@gmail.com>
2023-06-03 00:27:09 -0600

    Added condition and synthetic event

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Button from './button'
 
 const App = () => {
+    const myVariable = false;
+
+    if (myVariable) {
+        return <p>Mi variable dio true!</p>
+    }
     return (
         <div>
-            <h1> Hola mundo</h1>
-            <Button onClick={() => console.log('Clickeado')} >Enviar2</Button>
+            <h1 onClick={(e) => console.log("Click", e)}> Hola mundo</h1>
+            <Button onClick={() => console.log('Clickeado')} >
+                Enviar2
+            </Button>
         </div>
     )
 }
