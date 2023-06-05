3862f6d24d1ecc368f0d7ab3bc55049c771734a6
cesaroer <cesarvarcard@gmail.com>
2023-06-05 00:04:11 -0600

    Anatomia de componente basado en clases

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import Button from './button'
+import { Component } from 'react'
 
-const arr = [
-    'feliz',
-    'triste',
-    'emocionado',
-]
-
-const App = () => {
-    const myVariable = false;
-
-    if (myVariable) {
-        return <p>Mi variable dio true!</p>
+class App extends Component {
+    render() {
+        return (
+            <p>
+                Hola Mundo
+            </p>
+        )
     }
-    return (
-        <div>
-            <h1 onClick={(e) => console.log("Click", e)}> Hola mundo</h1>
-            {arr.map(e => <p key={e}>{e}</p>)}
-            <Button onClick={() => console.log('Clickeado')} >
-                Enviar2
-            </Button>
-        </div>
-    )
 }
 
 export default App
