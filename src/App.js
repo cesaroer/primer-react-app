c3de949435fa6bcd4e15a909561f10b9e0f5f2f2
cesaroer <cesarvarcard@gmail.com>
2023-06-05 00:31:59 -0600

    Added comments to know component life cycle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 import { Component } from 'react'
 
+class Btn extends Component {
+    state = {
+    }
+
+    constructor(props) {
+        // Se ejecuta primero
+        super(props)
+        console.log('Constructor', props);
+
+    }
+
+    componentDidMount() {
+        // se ejecuta ultimo
+        console.log('componentDidMount');
+    }
+
+    render() {
+        // se ejecuta en medio
+        console.log("Ejecutando metodo render de Btn")
+
+        return (
+            <button>
+                Evniar
+            </button>
+        )
+    }
+}
+
+
 class App extends Component {
 
     state = {
@@ -13,8 +42,9 @@ class App extends Component {
                 <p>
                     Hola Mundo
                 </p>
+                <Btn humor="feliz" />
                 <button
-                    className={"${this.state.valor}"}
+                    className={this.state.valor}
                     onClick={() => this.setState({ valor: 2 })}
                 >
                     Send
