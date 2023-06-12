dae578f5628c53e99872f220515458ee3d742705
cesaroer <cesarvarcard@gmail.com>
2023-06-12 00:06:25 -0600

    Added images, price and name

--- src/App.js
+++ src/App.js
@@ -1,41 +1,5 @@
 import { Component } from 'react'
-
-// class Btn extends Component {
-//     state = {
-//     }
-
-//     constructor(props) {
-//         // Se ejecuta primero
-//         super(props)
-//         console.log('Constructor', props);
-
-//     }
-
-//     componentDidMount() {
-//         // se ejecuta ultimo
-//         console.log('componentDidMount');
-//     }
-
-//     componentDidUpdate(prevProps, prevState) {
-//         console.log('componentDid-Update', prevProps, prevState);
-//     }
-
-//     componentWillUnmount() {
-//         console.log('componentWillUnmount', this.props, this.state);
-//     }
-
-//     render() {
-//         // se ejecuta en medio
-//         console.log("Ejecutando metodo render de Btn")
-
-//         return (
-//             <button onClick={() => this.setState({ prop: 1 })}>
-//                 Evniar
-//             </button>
-//         )
-//     }
-// }
-
+import Productos from './components/Productos'
 
 class App extends Component {
 
@@ -51,9 +15,10 @@ class App extends Component {
         console.log(this.state)
         return (
             <div>
-                <p>
-                    Hola Mundo
-                </p>
+                <Productos
+                    agregarAlCarro={() => console.log('No hace nada')}
+                    productos={this.state.productos}
+                />
             </div>
         )
     }
