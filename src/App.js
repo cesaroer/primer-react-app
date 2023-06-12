850851430137ee7a7226f8645028272c53f17eaf
cesaroer <cesarvarcard@gmail.com>
2023-06-11 23:48:51 -0600

    starting with carrito de compras

--- src/App.js
+++ src/App.js
@@ -1,47 +1,44 @@
 import { Component } from 'react'
 
-class Btn extends Component {
-    state = {
-    }
+// class Btn extends Component {
+//     state = {
+//     }
 
-    constructor(props) {
-        // Se ejecuta primero
-        super(props)
-        console.log('Constructor', props);
+//     constructor(props) {
+//         // Se ejecuta primero
+//         super(props)
+//         console.log('Constructor', props);
 
-    }
+//     }
 
-    componentDidMount() {
-        // se ejecuta ultimo
-        console.log('componentDidMount');
-    }
+//     componentDidMount() {
+//         // se ejecuta ultimo
+//         console.log('componentDidMount');
+//     }
 
-    componentDidUpdate(prevProps, prevState) {
-        console.log('componentDid-Update', prevProps, prevState);
-    }
+//     componentDidUpdate(prevProps, prevState) {
+//         console.log('componentDid-Update', prevProps, prevState);
+//     }
 
-    componentWillUnmount() {
-        console.log('componentWillUnmount', this.props, this.state);
-    }
+//     componentWillUnmount() {
+//         console.log('componentWillUnmount', this.props, this.state);
+//     }
 
-    render() {
-        // se ejecuta en medio
-        console.log("Ejecutando metodo render de Btn")
+//     render() {
+//         // se ejecuta en medio
+//         console.log("Ejecutando metodo render de Btn")
 
-        return (
-            <button onClick={() => this.setState({ prop: 1 })}>
-                Evniar
-            </button>
-        )
-    }
-}
+//         return (
+//             <button onClick={() => this.setState({ prop: 1 })}>
+//                 Evniar
+//             </button>
+//         )
+//     }
+// }
 
 
 class App extends Component {
 
-    state = {
-        valor: 3
-    }
 
     render() {
         console.log(this.state)
@@ -50,15 +47,6 @@ class App extends Component {
                 <p>
                     Hola Mundo
                 </p>
-                {this.state.valor === 3
-                    ? <Btn humor="feliz" />
-                    : null}
-                <button
-                    className={this.state.valor}
-                    onClick={() => this.setState({ valor: 2 })}
-                >
-                    Send
-                </button>
             </div>
         )
     }
